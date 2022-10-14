639ab4cff6851b72f88babac642d2f7c1a7cff0e
Sean Moloney <sean.moloney@goswift.ly>
2022-10-14 12:48:36 -0700

    testing java

--- src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
+++ src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
@@ -11,9 +11,11 @@ public class WorksWithHerokuServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/plain");
-        response.setStatus(404);
+        response.setStatus(404)
         PrintWriter writer = response.getWriter();
         writer.print("Buddy Works with Heroku");
         writer.close();
+
+        fooobaaaarr
     }
 }
